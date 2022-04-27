5fefa7cbd49432ad52bbfec885f860e88190c858
WmmyS <wesleymarcosm@gmail.com>
2022-04-27 00:23:01 -0300

    feat: Adicionada aula de classes.

---
+++ src/Aula21-classes/classes.ts
@@ -0,0 +1,46 @@
+export class Empresa {
+  public readonly nome: string; // public não necessário
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  // Constructor menos rápido
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+  // public pode ser implícito
+  public mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  //Constructor mais rápido
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+// Instanciando as classes
+const empresa = new Empresa('WWWSolucoes', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Wesley', 'Santos');
+const colaborador2 = new Colaborador('Walyson', 'Santos');
+const colaborador3 = new Colaborador('Wedson', 'Santos');
+
+//Utilizando métodos públicos para adicionar elementos
+empresa.adicionaColaborador(colaborador1);
+empresa.adicionaColaborador(colaborador2);
+empresa.adicionaColaborador(colaborador3);
+
+// Adicionar elemento utilizando o structure type
+// json?
+empresa.adicionaColaborador({ nome: 'Nubia', sobrenome: 'Pires' });
+
+empresa.mostrarColaboradores();
