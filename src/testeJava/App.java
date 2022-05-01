1994833cd4c98bed9b84cc2cb70c071fd097cba1
WmmyS <wesleymarcosm@gmail.com>
2022-05-01 19:58:49 -0300

    feat: Adição de aulas.

---
+++ src/testeJava/App.java
@@ -0,0 +1,10 @@
+package testeJava;
+
+public class App {
+
+public static void main(String[] args) {
+    Application application = new Application("App", "1", "Aplicação");
+    System.out.println(application.getNome()+ " ");
+}
+
+}
